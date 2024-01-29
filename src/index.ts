e3b6771b9e96871a9a1610b896763d55afa717a9
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2024-01-29 09:12:05 +0100

    feat: multiple images and custom URL_PREFIX

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,44 @@
-import fs from 'fs';
-import Axios from 'axios';
+import fs from "fs";
+import Axios from "axios";
 
-const portainerUrl      = process.env.PLUGIN_PORTAINER_URL;
+const branchName = process.env.DRONE_BRANCH;
+const commitHash = process.env.DRONE_COMMIT_SHA;
+
+const portainerUrl = process.env.PLUGIN_PORTAINER_URL;
 const portainerUsername = process.env.PLUGIN_PORTAINER_USERNAME;
 const portainerPassword = process.env.PLUGIN_PORTAINER_PASSWORD;
-const registry          = process.env.PLUGIN_REGISTRY;
-const image             = process.env.PLUGIN_IMAGE;
-const imageTag          = process.env.PLUGIN_IMAGE_TAG;
-const stackName         = process.env.PLUGIN_STACK_NAME;
-const endpoint          = process.env.PLUGIN_ENDPOINT;
-const composeEnvStr     = process.env.PLUGIN_COMPOSE_ENVIRONMENT;
-let   dockerComposeFile = process.env.PLUGIN_COMPOSE_FILE;
-const standalone        = process.env.PLUGIN_STANDALONE;
-const forcePull         = process.env.PLUGIN_FORCE_PULL;
+const registry = process.env.PLUGIN_REGISTRY;
+const images = process.env.PLUGIN_IMAGES;
+const stackName = process.env.PLUGIN_STACK_NAME;
+const endpoint = process.env.PLUGIN_ENDPOINT;
+const composeEnvStr = process.env.PLUGIN_COMPOSE_ENVIRONMENT;
+let dockerComposeFile = process.env.PLUGIN_COMPOSE_FILE;
+const standalone = process.env.PLUGIN_STANDALONE;
+const forcePull = process.env.PLUGIN_FORCE_PULL;
 
 let additionalComposeEnv: { [key: string]: string } = {};
 
-if (composeEnvStr && composeEnvStr !== '') {
+if (composeEnvStr && composeEnvStr !== "") {
     additionalComposeEnv = JSON.parse(composeEnvStr);
 }
 
-if (!dockerComposeFile || dockerComposeFile === '') {
+if (!dockerComposeFile || dockerComposeFile === "") {
     dockerComposeFile = "docker-compose.yml";
 }
-
-const imageName = `${registry}/${image}`;
 
 const axios = Axios.create({
     baseURL: `${portainerUrl}/api`,
     validateStatus: function(status) {
         return status < 5000;
-    }
+    },
 });
 
 (async function() {
-
     // Authenticate with portainer and set the bearer token
-    let response = await axios.post("/auth", { Username: portainerUsername, Password: portainerPassword });
+    let response = await axios.post("/auth", {
+        Username: portainerUsername,
+        Password: portainerPassword,
+    });
 
     if (response.status !== 200) {
         console.error("Login failed");
@@ -45,7 +47,7 @@ const axios = Axios.create({
     }
 
     const bearerToken = response.data.jwt as string;
-    axios.defaults.headers.common['Authorization'] = "Bearer " + bearerToken;
+    axios.defaults.headers.common["Authorization"] = "Bearer " + bearerToken;
 
     const endpointsReponse = await axios.get("/endpoints");
 
@@ -55,15 +57,15 @@ const axios = Axios.create({
         process.exit(1);
     }
 
-
     // Find the endpoint id
-    const localEp = endpointsReponse.data.find((ep: { Id: number, Name: string }) => ep.Name === endpoint);
+    const localEp = endpointsReponse.data.find(
+        (ep: { Id: number; Name: string }) => ep.Name === endpoint,
+    );
 
-    if (!localEp){
+    if (!localEp) {
         console.error(`Endpoint ${endpoint} not found`);
         process.exit(1);
     }
-
 
     // Check if the private registry is registered with portainer
     const registriesResponse = await axios.get("/registries");
@@ -74,37 +76,50 @@ const axios = Axios.create({
         process.exit(1);
     }
 
-    const registryFromList = registriesResponse.data.find((reg: { Id: number, URL: string }) => reg.URL === registry);
+    const registryFromList = registriesResponse.data.find(
+        (reg: { Id: number; URL: string }) => reg.URL === registry,
+    );
 
     if (!registryFromList) {
         console.error("Registry not configured in portainer");
         process.exit(1);
     }
 
-
     // Supply a 'X-Registry-Auth' header to work with portainer
     const xRegistryAuth = { registryId: registryFromList.Id };
-    const xRegistryAuthStr = Buffer.from(JSON.stringify(xRegistryAuth)).toString("base64");
+    const xRegistryAuthStr = Buffer.from(JSON.stringify(xRegistryAuth)).toString(
+        "base64",
+    );
 
-    // Pull the image
-    const imageResponse = await axios.post(`/endpoints/${localEp.Id}/docker/images/create`, { }, 
-        {
-            headers: { "X-Registry-Auth": xRegistryAuthStr },
-            params: { fromImage: imageName, tag: imageTag }
-        });
+    const releaseTag = branchName + commitHash.substr(0, 8);
 
-    if (imageResponse.status !== 200){
-        console.error("Could not pull image");
-        console.error(imageResponse);
-        process.exit(1);
-    }
+    await Promise.all(
+        images.split(",").map(async (imageName: string) => {
+            // Pull the image
+            const imageResponse = await axios.post(
+                `/endpoints/${localEp.Id}/docker/images/create`,
+                {},
+                {
+                    headers: { "X-Registry-Auth": xRegistryAuthStr },
+                    params: { fromImage: imageName, tag: releaseTag },
+                },
+            );
 
+            if (imageResponse.status !== 200) {
+                console.error("Could not pull image " + imageName);
+                console.error(imageResponse);
+                process.exit(1);
+            }
+        }),
+    );
 
     let stackOptions = {};
     let swarmId = "";
     if (!standalone) {
         // Find the swarm id
-        const swarmResponse = await axios.get(`/endpoints/${localEp.Id}/docker/swarm`);
+        const swarmResponse = await axios.get(
+            `/endpoints/${localEp.Id}/docker/swarm`,
+        );
 
         if (swarmResponse.status !== 200) {
             console.error("Could not get swarm id");
@@ -116,7 +131,7 @@ const axios = Axios.create({
 
         console.log(`Swarm id: ${swarmId}`);
         stackOptions = {
-            params: { filters: { SwarmID: swarmId } }
+            params: { filters: { SwarmID: swarmId } },
         };
     }
 
@@ -130,11 +145,13 @@ const axios = Axios.create({
     }
 
     // Update the stack
-    const stackToUpdate = stacksResponse.data.find((stack: { Id: Number, Name: string }) => stack.Name === stackName);
+    const stackToUpdate = stacksResponse.data.find(
+        (stack: { Id: Number; Name: string }) => stack.Name === stackName,
+    );
 
     // Read docker-compose.yml
     let composeFile: Buffer;
-    
+
     try {
         composeFile = fs.readFileSync(dockerComposeFile);
     } catch (e) {
@@ -143,13 +160,18 @@ const axios = Axios.create({
         process.exit(1);
     }
 
+    let urlPrefix = branchName == "main" ? "" : branchName + ".";
+
     let composeEnvArray = [
-        { "name": "imageName", "value": `${imageName}:${imageTag}` },
-        { "name": "stackName", "value": stackName }
+        { name: "RELEASE_TAG", value: releaseTag },
+        { name: "URL_PREFIX", value: urlPrefix },
+        { name: "STACK_NAME", value: stackName },
     ];
 
     if (additionalComposeEnv) {
-        Object.keys(additionalComposeEnv).forEach(k => composeEnvArray.push({ "name": k, "value": additionalComposeEnv[k] }));
+        Object.keys(additionalComposeEnv).forEach((k) =>
+            composeEnvArray.push({ name: k, value: additionalComposeEnv[k] }),
+        );
     }
 
     if (!stackToUpdate) {
@@ -160,16 +182,19 @@ const axios = Axios.create({
             Name: stackName,
             StackFileContent: composeFile.toString(),
             Env: composeEnvArray,
-            Prune: true
+            Prune: true,
         };
 
         if (!standalone) {
             operationType = 1;
-            const swarmOptions = {SwarmID: swarmId};
-            stackCreateOptions = {...stackCreateOptions, ...swarmOptions};
+            const swarmOptions = { SwarmID: swarmId };
+            stackCreateOptions = { ...stackCreateOptions, ...swarmOptions };
         }
 
-        const stackCreateResponse = await axios.post(`/stacks?type=${operationType}&method=string&endpointId=${localEp.Id}`, stackCreateOptions);
+        const stackCreateResponse = await axios.post(
+            `/stacks?type=${operationType}&method=string&endpointId=${localEp.Id}`,
+            stackCreateOptions,
+        );
 
         if (stackCreateResponse.status !== 200) {
             console.error("Could not create stack");
@@ -179,14 +204,16 @@ const axios = Axios.create({
     } else {
         console.log(`Updating stack ${stackToUpdate.Id} - ${stackToUpdate.Name}`);
 
-        const stackUpdateResponse = await axios.put(`/stacks/${stackToUpdate.Id}?endpointId=${localEp.Id}`,
+        const stackUpdateResponse = await axios.put(
+            `/stacks/${stackToUpdate.Id}?endpointId=${localEp.Id}`,
             {
                 id: stackToUpdate.Id,
                 StackFileContent: composeFile.toString(),
                 Env: composeEnvArray,
                 Prune: true,
                 PullImage: !!forcePull,
-            });
+            },
+        );
 
         if (stackUpdateResponse.status !== 200) {
             console.error("Could not update stack");
